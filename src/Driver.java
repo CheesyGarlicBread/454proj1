b7d42ddbf8467c878abe8f3cadb8631ac04a7975
Harris Chu <harrischu@gmail.com>
2011-06-10 16:38:07 -0400

    added driver code

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,18 @@
 import java.io.*;
+import java.util.StringTokenizer;
 public class Driver {
 	final int chunkSize = 65536;
 	final int maxPeers = 6;
-	final int maxFiles = 100; 
+	final int maxFiles = 100;
+	
+	private static Peer peer;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		peer = new Peer();
 		while(true){
 			System.out.println("Please enter a command:");
 			try{
@@ -24,7 +28,8 @@ public class Driver {
 	}
 	
 	public static void parseCommand(String command){
-		
+		System.out.println("You sent in: " + command);
+		StringTokenizer st = new StringTokenizer(command, " ");
 	}
 
 }
