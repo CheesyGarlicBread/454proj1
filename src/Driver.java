a1f7d0fb2201055f379e33e389431f2b5007ea2f
Harris Chu <harrischu@gmail.com>
2011-06-10 14:41:31 -0400

    Added driver

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+
+public class Driver {
+	final int chunkSize = 65536;
+	final int maxPeers = 6;
+	final int maxFiles = 100; 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
