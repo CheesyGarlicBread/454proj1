0b8d8dc3325dd79ce837e59bc290e8f842de4ae2
Kieran Crawford <kierancrawford@gmail.com>
2011-06-10 16:35:40 -0400

    comments

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,15 @@
 
 public class Driver {
+	
 	final int chunkSize = 65536;
 	final int maxPeers = 6;
 	final int maxFiles = 100; 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		
 	}
 
 }
--- src/Status.java
+++ src/Status.java
@@ -1,4 +1,55 @@
 
 public class Status {
-
+	
+	final int chunkSize = 65536;
+	final int maxPeers  = 6;
+	final int maxFiles  = 100;
+	
+	// This is very cheesy and very lazy, but the focus of this assignment
+    // is not on dynamic containers but on the BT p2p file distribution
+	
+	// The number of files currently in the system, as viewed by this peer
+	private int numFiles;
+	
+	// The fraction of the file present locally 
+    // (= chunks on this peer/total number chunks in the file)
+	private double local[] = new double[maxFiles];
+	
+	// The fraction of the file present in the system 
+    // (= chunks in the system/total number chunks in the file)
+    // (Note that if a chunk is present twice, it doesn't get counted twice;
+    // this is simply intended to find out if we have the whole file in
+    // the system; given that a file must be added at a peer, think about why
+    // this number would ever not be 1.)
+	private double system[] = new double[maxFiles];
+	
+	// Sum by chunk over all peers; the minimum of this number is the least 
+    // replicated chunk, and thus represents the least level of replication
+    // of the file
+	private int leastReplication[] = new int[maxFiles];
+	
+	// Sum all chunks in all peers; divide this by the number of chunks in the
+    // file; this is the average level of replication of the file
+	private double weightedLeastReplication[] = new double[maxFiles];
+	
+	public int numberOfFiles()
+	{
+		return 0;
+	}
+	public double fractionPresentLocally(int fileNumber)
+	{
+		return 0;
+	}
+	public double fractionPresent(int fileNumber)
+	{
+		return 0;
+	}
+	public int   minimumReplicationLevel(int fileNumber)
+	{
+		return 0;
+	}
+	public double averageReplicationLevel(int fileNumber)
+	{
+		return 0;
+	}	
 }
