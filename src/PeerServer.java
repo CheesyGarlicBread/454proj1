814784d481d226fa7ccb093ae6b02111292d4620
Harris Chu <harrischu@gmail.com>
2011-06-11 19:05:04 -0400

    stuff

--- src/PeerServer.java
+++ src/PeerServer.java
@@ -6,7 +6,7 @@ public class PeerServer implements Runnable{
 	public void run(){
 		 try {
 			 System.out.println("Started Server");
-		     
+			 java.rmi.registry.LocateRegistry.createRegistry(1099);
 		     Naming.rebind("rmi://localhost:1099/PeerService", pi);
 		 } catch (MalformedURLException e) {
 		     System.out.println("Malformed URL: " + e);
